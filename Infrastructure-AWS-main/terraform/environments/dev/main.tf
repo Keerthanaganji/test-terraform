ac1f4afc5733d6c83c953d25fd601c2c224cf423
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-17 22:01:19 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -1,19 +1,10 @@
-
-#### Postgress ####
 provider "aws" {
   region = var.region
 }
 
-module "postgres_instance" {
-  source = "../../modules/postgres"
+module "s3_bucket" {
+  source = "../../modules/s3_bucket"
 
-  db_name             = var.db_name
-  db_password         = var.db_password
-  db_engine_version   = var.db_engine_version
-  master_name         = var.master_name
-  deletion_protection = var.deletion_protection
-  network_type        = var.network_type
-  cluster_id          = var.cluster_id
+  bucket = var.bucket_name
 }
-
 
