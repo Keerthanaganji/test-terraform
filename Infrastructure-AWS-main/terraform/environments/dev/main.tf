e56dcee4790bf036cd653fe9274cdbe20eef6a94
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-17 19:06:12 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -1,26 +1,4 @@
-provider "aws" {
-  region = var.region
-}
 
-module "s3_bucket" {
-  source = "../../modules/s3_bucket"
-
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name_first
-}
-  
-} 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = module.s3_bucket.bucket_id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = module.s3_bucket.bucket_id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
 #### Postgress ####
 provider "aws" {
   region = var.region
@@ -32,7 +10,6 @@ module "postgres_instance" {
   db_name            = var.db_name
   db_password           = var.db_password
   db_engine_version  = var.db_engine_version
-  db_parameter_group = var.db_parameter_group
   master_name        = var.master_name
   deletion_protection = var.deletion_protection
   network_type   = var.network_type
