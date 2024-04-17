c17fdd06e7085665bac648314c65c43a2f5591f4
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-17 22:12:22 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
@@ -3,12 +3,12 @@ resource "aws_s3_bucket" "test" {
 }
 
 resource "aws_s3_bucket_acl" "test" {
-  bucket = hellosampletest.test.id
+  bucket = mysamplebuckettest.test.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = hellosampletest.test.id
+  bucket = mysamplebuckettest.test.id
   versioning_configuration {
     status = "Enabled"
   }
