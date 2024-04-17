55805a11e6ec8b30e7ffbcbd3e81a0b17a9083b9
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-17 14:41:10 +0530

    Delete terraform directory

--- terraform/environments/dev/dev.tfvars
+++
@@ -1,4 +0,0 @@
-bucket_name_first = "mysamplebuckethello"
-bucket_name_second = "mysamplebucketdemobye"
-
-region = "eu-west-2"
--- terraform/modules/s3_bucket/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name_first" {
-  type = string
-  description = "Name of the first s3 Bucketb"
-}  
-
-variable "bucket_name_second" {
-  type = string
-  description = "Name of the second s3 Bucketb"
-}
