2dc9efacef23b547863ba0eeb6e12a6549b2ebd4
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-17 22:41:43 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "test" {
+resource "aws_s3_bucket" "bucket_name" {
   bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.test.id
+  bucket = aws_s3_bucket.bucket_name.id
   versioning_configuration {
     status = "Enabled"
   }
