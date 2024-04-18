34366f907d891202f50517d653c39536020df7c3
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 11:00:51 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -1,12 +1,12 @@
 resource "aws_glue_job" "this" {
-  name                   = var.full_name
+  full_name                   = var.full_name
   role_arn               = var.create_role 
   connections            = var.connections
   description            = var.description
   glue_version           = var.glue_version
   max_retries            = var.max_retries
   timeout                = var.timeout
-  security_configuration = var.create_security_configuration
+  create_security_configuration = var.create_security_configuration
   worker_type            = var.worker_type
   number_of_workers      = var.number_of_workers
 }
