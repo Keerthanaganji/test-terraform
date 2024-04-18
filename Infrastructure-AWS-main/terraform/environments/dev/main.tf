f43de1e22cf30c099df7e802428ef1ee802e9832
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 11:04:28 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -15,14 +15,14 @@ module "aws_s3_bucket" {
 module "aws_glue_job" {
   source = "../../modules/glue_job"
 
-  full_name              = var.full_name
+  name              = var.name
   role_arn               = var.create_role 
   connections            = var.connections
   description            = var.description
   glue_version           = var.glue_version
   max_retries            = var.max_retries
   timeout                = var.timeout
-  create_security_configuration = var.create_security_configuration
+  security_configuration = var.security_configuration
   worker_type            = var.worker_type
   number_of_workers      = var.number_of_workers
  }
