db96d0f5ef848b4a763ec455ee899b2b3f382ebf
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 17:03:59 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
@@ -3,28 +3,48 @@ variable "db_name" {
   default     = "exampledb"
   type = string
 }
-
-variable "db_password" {
-  description = "The password for accessing the PostgreSQL database"
-  default     = "your_password"
-  type = string
+variable "engine" {
+  description = "The name of the database engine to be used for this DB cluster. Defaults to `aurora`. Valid Values: `aurora`, `aurora-mysql`, `aurora-postgresql`"
+  type        = string
+  default     = null
 }
 
-variable "db_engine_version" {
-  description = "The engine version for the PostgreSQL instance"
-  default     = "12.5"
-  type = string
+variable "engine_mode" {
+  description = "The database engine mode. Valid values: `global`, `multimaster`, `parallelquery`, `provisioned`, `serverless`. Defaults to: `provisioned`"
+  type        = string
+  default     = "provisioned"
 }
 
-variable "deletion_protection" {
+variable "engine_version" {
   description = "The engine version for the PostgreSQL instance"
+  default     = "15.6"
   type = string
 }
-variable "network_type" {
-  description = "The engine version for the PostgreSQL instance"
-  type = string
+variable "db_subnet_group_name" {
+  description = "The name of the subnet group name (existing or created)"
+  type        = string
+  default     = ""
 }
-variable "cluster_id" {
-  description = "The engine version for the PostgreSQL instance"
-  type = string
+
+variable "private_subnets_cidr_blocks" {
+  description = "List of subnet IDs used by database subnet group created"
+  type        = list(string)
+  default     = []
+}
+
+variable "apply_immediately" {
+  description = "Specifies whether any cluster modifications are applied immediately, or during the next maintenance window. Default is `false`"
+  type        = bool
+  default     = null
+}
+variable "instance_class" {
+  description = "Instance type to use at master instance. Note: if `autoscaling_enabled` is `true`, this will be the same instance class used on instances created by autoscaling"
+  type        = string
+  default     = ""
+}
+
+variable "monitoring_interval" {
+  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for instances. Set to `0` to disable. Default is `0`"
+  type        = number
+  default     = 0
 }
