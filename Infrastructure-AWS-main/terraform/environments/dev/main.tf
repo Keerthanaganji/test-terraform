5cdc833dd4a8839fd16bd4899e605bcf1c1f6ddd
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 16:34:17 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -29,3 +29,46 @@ module "aws_glue_job" {
   python_version  = var.python_version
  }
 
+################################################################################
+# PostgreSQL Serverless v2
+################################################################################
+
+data "aws_rds_engine_version" "postgresql" {
+  engine  = "aurora-postgresql"
+  version = "14.5"
+}
+
+module "aurora_postgresql_v2" {
+  source = "../../module/postgres"
+
+  name              = "postgresqlv2"
+  engine            = data.aws_rds_engine_version.postgresql.engine
+  engine_mode       = "provisioned"
+  engine_version    = data.aws_rds_engine_version.postgresql.version
+  storage_encrypted = true
+  master_username   = "root"
+
+  vpc_id               = module.vpc.vpc_id
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+  security_group_rules = {
+    vpc_ingress = {
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks
+    }
+  }
+
+  monitoring_interval = 60
+
+  apply_immediately   = true
+  skip_final_snapshot = true
+
+  serverlessv2_scaling_configuration = {
+    min_capacity = 2
+    max_capacity = 10
+  }
+
+  instance_class = "db.serverless"
+  instances = {
+    one = {}
+    two = {}
+  }
+
