9d59cedf496249d1be346ac1e939881a9a7c8771
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 17:10:44 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/main.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/main.tf
@@ -1,13 +1,42 @@
-### Postgress ####
+################################################################################
+# PostgreSQL Serverless v2
+################################################################################
 
-resource ""aws_db_instance" "postgresql" {
-  source = "../../modules/postgres"
-
-  db_name             = var.db_name
-  db_password         = var.db_password
-  db_engine_version   = var.db_engine_version
-  master_name         = var.master_name
-  deletion_protection = var.deletion_protection
-  network_type        = var.network_type
-  cluster_id          = var.cluster_id
+data "aws_rds_engine_version" "postgresql" {
+  engine  = var.engine
+  engine_version = var.engine_version
 }
+
+module "aurora_postgresql_v2" {
+  source = "../../module/postgres"
+
+  db_name           = var.db_name
+  engine            = var.engine
+  engine_mode       = var.engine_mode
+  engine_version    = var.engine_version
+  storage_encrypted = true
+  master_username   = var.master_username
+
+  vpc_id               = var.vpc_id
+  db_subnet_group_name = var.db_subnet_group_name
+  security_group_rules = {
+    vpc_ingress = {
+      cidr_blocks = var.private_subnets_cidr_blocks
+    }
+  }
+
+  monitoring_interval = 60
+
+  apply_immediately   = true
+  skip_final_snapshot = true
+
+  serverlessv2_scaling_configuration = {
+    min_capacity = 2
+    max_capacity = 10
+  }
+
+  instance_class = "db.serverless"
+  instances = {
+    one = {}
+    two = {}
+  }
