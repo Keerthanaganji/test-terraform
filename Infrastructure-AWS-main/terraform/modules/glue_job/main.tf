d70deabc26ffd2052cae616aa2956244ce456b14
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 08:58:18 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -1,1 +1,17 @@
+resource "aws_glue_job" "this" {
+  name                   = local.full_name
+  role_arn               = var.create_role 
+  connections            = var.connections
+  description            = var.description
+  glue_version           = var.glue_version
+  max_retries            = var.max_retries
+  timeout                = var.timeout
+  worker_type            = var.worker_type
+  number_of_workers      = var.number_of_workers
+  tags                   = local.tags
 
+  command {
+    name            = "glueetl"
+    script_location = var.script_location
+    python_version  = var.python_version
+  }
