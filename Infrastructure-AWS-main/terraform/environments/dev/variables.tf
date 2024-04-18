d3065b13984ae0396d9fe9e52481f9e80b7ba5af
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 09:31:27 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/environments/dev/variables.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/variables.tf
@@ -39,6 +39,51 @@ variable "cluster_id" {
   type = string
 }
 
+variable "name" {
+  description = "The name of the Glue job"
+}
+
+variable "create_role" {
+  description = "Whether to create a new IAM role for the Glue job"
+  default     = false
+}
+
+variable "role_arn" {
+  description = "The ARN of an existing IAM role for the Glue job"
+}
+
+variable "connections" {
+  description = "The list of connections used by the Glue job"
+}
+
+variable "description" {
+  description = "The description of the Glue job"
+}
+
+variable "glue_version" {
+  description = "The Glue version to be used for the job"
+}
+
+variable "max_retries" {
+  description = "The maximum number of times to retry the job if it fails"
+}
+
+variable "timeout" {
+  description = "The job timeout in minutes"
+}
+
+variable "security_configuration" {
+  description = "The ID of an existing security configuration for the Glue job"
+}
+
+variable "worker_type" {
+  description = "The type of worker to use for the Glue job"
+}
+
+variable "number_of_workers" {
+  description = "The number of workers to allocate for the Glue job"
+}
+
 
 
 
