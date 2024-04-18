5dc5685ee35a44e0f5e5cdb925f70addd6a81928
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 16:23:10 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -25,10 +25,7 @@ module "aws_glue_job" {
   security_configuration = var.security_configuration
   worker_type            = var.worker_type
   number_of_workers      = var.number_of_workers
-
-   command {
-    script_location = var.script_location 
-    python_version  = var.python_version
-  }
+  script_location = var.script_location 
+  python_version  = var.python_version
  }
 
