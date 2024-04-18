f65f3a9fe0122042f4ab9312321640d9923ace91
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 07:33:48 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/main.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/main.tf
@@ -1,38 +1,12 @@
-data "aws_rds_engine_version" "postgresql" {
-  engine  = "aurora-postgresql"
-  version = "14.5"
-}
+### Postgress ####
 
-module "aurora_postgresql_v2" {
-  source = "../../"
+resource "postgres" {
+  source = "../../modules/postgres"
 
-  name              = "postgresqlv2"
-  engine            = aws_rds_engine_version.postgresql.engine
-  engine_mode       = "provisioned"
-  engine_version    = aws_rds_engine_version.postgresql.version
-  storage_encrypted = true
-  master_username   = "root"
-
-  vpc_id               = module.vpc.vpc_id
-  db_subnet_group_name = module.vpc.database_subnet_group_name
-  security_group_rules = {
-    vpc_ingress = {
-      cidr_blocks = module.vpc.private_subnets_cidr_blocks
-    }
-  }
-
-  monitoring_interval = 60
-
-  apply_immediately   = true
-  skip_final_snapshot = true
-
-  serverlessv2_scaling_configuration = {
-    min_capacity = 2
-    max_capacity = 10
-  }
-
-  instance_class = "db.serverless"
-  instances = {
-    one = {}
-    two = {}
-  }
+  db_name             = var.db_name
+  db_password         = var.db_password
+  db_engine_version   = var.db_engine_version
+  master_name         = var.master_name
+  deletion_protection = var.deletion_protection
+  network_type        = var.network_type
+  cluster_id          = var.cluster_id
