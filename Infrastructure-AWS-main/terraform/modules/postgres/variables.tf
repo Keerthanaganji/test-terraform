6a0c78ca8ee1ee77431498f7a4876abf0a614e33
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 07:37:32 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
@@ -1,1 +1,25 @@
-
+variable "db_name" {
+  description = "The name of the database to create within the PostgreSQL instance"
+  default     = "exampledb"
+  type = string
+}
+variable "db_name" {
+  description = "The name of the database to create within the PostgreSQL instance"
+  default     = "exampledb"
+  type = string
+}
+variable "db_name" {
+  description = "The name of the database to create within the PostgreSQL instance"
+  default     = "exampledb"
+  type = string
+}
+variable "db_name" {
+  description = "The name of the database to create within the PostgreSQL instance"
+  default     = "exampledb"
+  type = string
+}
+variable "db_name" {
+  description = "The name of the database to create within the PostgreSQL instance"
+  default     = "exampledb"
+  type = string
+}
