e05b089bcd6a345d52e476338c7a01134009d6ce
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-18 09:23:38 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -7,20 +7,7 @@ module "aws_s3_bucket" {
 
   bucket_name = var.bucket_name
 }
-#####################################################################################
-### Postgress ####
-########################################################################################
-module "aws_db_instance"{
-  source = "../../modules/postgres"
 
-  db_name             = var.db_name
-  db_password         = var.db_password
-  db_engine_version   = var.db_engine_version
-  master_name         = var.master_name
-  deletion_protection = var.deletion_protection
-  network_type        = var.network_type
-  cluster_id          = var.cluster_id
-}
 ######################################################################################################
 ### glue Job ###
 ####################################################################################################
