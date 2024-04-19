92f6d3420a0dee30d8b9b8d92bddfe302602e6d7
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-19 18:36:50 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/environments/dev/variables.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/variables.tf
@@ -2,7 +2,12 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "bucket_name" {
+variable "raw_bucket_name" {
+  type = string
+  description = "Name of the first S3 Bucket"
+}
+
+variable "artifact_bucket_name" {
   type = string
   description = "Name of the first S3 Bucket"
 }
