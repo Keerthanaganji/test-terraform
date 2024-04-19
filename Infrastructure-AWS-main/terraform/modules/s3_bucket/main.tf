2487ea0a091570822288427b190dc14fcf180d02
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-19 19:05:01 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name_first
 }
  
-resource "aws_s3_versioning_bucket" "versioning-sample" {
+resource "aws_s3_bucket_versioning" "versioning-sample" {
  
  bucket = var.bucket_name_second
   versioning_configuration {
