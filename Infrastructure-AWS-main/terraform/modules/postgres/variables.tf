27478eaed2b531b03b75ed6edc46716bd6df8bd5
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-19 14:26:53 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
@@ -15,6 +15,18 @@ variable "id" {
   default     = ""
 }
 
+variable "master_username" {
+  description = "Username for the master DB user. Required unless `snapshot_identifier` or `replication_source_identifier` is provided or unless a `global_cluster_identifier` is provided when the cluster is the secondary cluster of a global database"
+  type        = string
+  default     = null
+}
+
+variable "skip_final_snapshot" {
+  description = "Determines whether a final snapshot is created before the cluster is deleted. If true is specified, no snapshot is created"
+  type        = bool
+  default     = false
+}
+
 variable "engine_mode" {
   description = "The database engine mode. Valid values: `global`, `multimaster`, `parallelquery`, `provisioned`, `serverless`. Defaults to: `provisioned`"
   type        = string
