fe6fe1f4c233b28cb9e2690f1bbc43ee0cf0d32f
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-23 16:51:46 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
@@ -1,74 +1,27 @@
-variable "database_name" {
-  description = "The name of the database to create within the PostgreSQL instance"
-  default     = "exampledb"
-  type = string
-}
-variable "engine" {
-  description = "The name of the database engine to be used for this DB cluster. Defaults to `aurora`. Valid Values: `aurora`, `aurora-mysql`, `aurora-postgresql`"
-  type        = string
-  default     = null
-}
+# modules/postgresql/variables.tf
 
-variable "id" {
-  description = "ID of the VPC where to create security group"
-  type        = string
-  default     = ""
-}
-
-variable "master_username" {
-  description = "Username for the master DB user. Required unless `snapshot_identifier` or `replication_source_identifier` is provided or unless a `global_cluster_identifier` is provided when the cluster is the secondary cluster of a global database"
-  type        = string
-  default     = null
-}
-
-variable "skip_final_snapshot" {
-  description = "Determines whether a final snapshot is created before the cluster is deleted. If true is specified, no snapshot is created"
-  type        = bool
-  default     = false
-}
-
-variable "engine_mode" {
-  description = "The database engine mode. Valid values: `global`, `multimaster`, `parallelquery`, `provisioned`, `serverless`. Defaults to: `provisioned`"
-  type        = string
-  default     = "provisioned"
-}
-
-variable "engine_version" {
-  description = "The engine version for the PostgreSQL instance"
-  default     = "15.6"
-  type = string
-}
 variable "db_subnet_group_name" {
-  description = "The name of the subnet group name (existing or created)"
-  type        = string
-  default     = ""
-}
-
-variable "cidr_block" {
-  description = "List of subnet IDs used by database subnet group created"
+  description = "Name for the DB subnet group"
   type        = string
 }
 
-variable "apply_immediately" {
-  description = "Specifies whether any cluster modifications are applied immediately, or during the next maintenance window. Default is `false`"
-  type        = bool
-  default     = null
+variable "subnet_ids" {
+  description = "List of subnet IDs for the DB subnet group"
+  type        = list(string)
 }
 
-variable "allocated_storage" {
-  description = "The amount of storage in gibibytes (GiB) to allocate to each DB instance in the Multi-AZ DB cluster. (This setting is required to create a Multi-AZ DB cluster)"
-  type        = number
-  default     = null
+variable "security_group_name" {
+  description = "Name for the PostgreSQL security group"
+  type        = string
 }
 
-variable "max_allocated_storage" {
-  description = "The amount of storage in gibibytes (GiB) to allocate to each DB instance in the Multi-AZ DB cluster. (This setting is required to create a Multi-AZ DB cluster)"
-  type        = number
-  default     = null
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
 }
 
-variable "monitoring_interval" {
-  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for instances. Set to `0` to disable. Default is `0`"
-  type        = number
-  default     = 0
+variable "db_instance_identifier" {
+  description = "Identifier for the DB instance"
+  type        = string
 }
+
