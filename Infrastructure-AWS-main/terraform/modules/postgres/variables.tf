e9f81e628d5c59734ba57039897cd0a5f3e18713
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-23 17:20:59 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/variables.tf
@@ -7,7 +7,7 @@ variable "db_subnet_group_name" {
 
 variable "subnet_ids" {
   description = "List of subnet IDs for the DB subnet group"
-  type        = list(string)
+  type        = string
 }
 
 variable "security_group_name" {
