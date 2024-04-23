ade4d523a6da5f3b3c7751e460d0d492b33195bd
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-23 17:01:30 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/main.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/main.tf
@@ -1,12 +1,12 @@
 resource "aws_db_subnet_group" "example" {
   name        = "test"
-  subnet_ids  = subnet-0c6df1696416a3fb4
+  subnet_ids  = "subnet-0c6df1696416a3fb4"
 }
 
 resource "aws_security_group" "example" {
-  name        = testvpckey
+  name        = "testvpckey"
   description = "Security group for PostgreSQL"
-  vpc_id      = vpc-0c62dcc69ca138dc0
+  vpc_id      = "vpc-0c62dcc69ca138dc0"
 }
 
 resource "aws_db_instance" "example" {
