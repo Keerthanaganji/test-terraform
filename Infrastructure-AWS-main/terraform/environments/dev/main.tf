8ec871725e469a359074f82c9357eb80cb0c99e7
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-24 18:22:25 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -34,26 +34,6 @@ provider "aws" {
 ################################################################################################
 module "postgresql_cluster" {
   source = "../../modules/postgres"
-
-  cluster_identifier      = "demo-cluster-member-1"
-  engine                  = "aurora-postgresql"
-  engine_version          = "15.4"
-  database_name           = "mydb"
-  master_username         = "user"
-  master_password         = "admin@123"
-  #backup_retention_period = 5
-  #preferred_backup_window = "07:00-09:00"
-  vpc_security_group_ids  = "vpc-07832f2f1eb8d75eb"
-  db_subnet_group_name    = "my-db-subnet-group"
-  count                   = 2
-  identifier              = "demo-cluster-member-instance-${count.index + 1}"
-  instance_class          = "db.r5.large"
-  publicly_accessible     = false
-  apply_immediately       = true
-  name                    = "clustervpc"
-  vpc_id           = "vpc-07832f2f1eb8d75eb"
-  group_name       = "clustergroup"
-  subnet_ids = ["subnet-03a665b3db2978814","subnet-0731f902c06ec204c"]
   
 }
 
