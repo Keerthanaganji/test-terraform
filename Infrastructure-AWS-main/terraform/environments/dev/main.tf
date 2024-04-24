0b007f0730d21f8320301f49533906b13c057724
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-24 16:07:18 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -2,32 +2,32 @@ provider "aws" {
   region = var.region
 }
 
-module "s3_bucket" {
-  source = "../../modules/s3_bucket"
+#module "s3_bucket" {
+#  source = "../../modules/s3_bucket"
  
-  bucket_name_first = var.bucket_name_first
-  bucket_name_second = var.bucket_name_second
-}
+#  bucket_name_first = var.bucket_name_first
+#  bucket_name_second = var.bucket_name_second
+#}
 
 ######################################################################################################
 ### glue Job ###
 ####################################################################################################
 
-module "aws_glue_job" {
-  source = "../../modules/glue_job"
+#module "aws_glue_job" {
+#  source = "../../modules/glue_job"
 
-  name              = var.name
-  connections            = var.connections
-  description            = var.description
-  glue_version           = var.glue_version
-  max_retries            = var.max_retries
-  timeout                = var.timeout
-  security_configuration = var.security_configuration
-  worker_type            = var.worker_type
-  number_of_workers      = var.number_of_workers
-  script_location = var.script_location 
-  python_version  = var.python_version
- }
+#  name              = var.name
+#  connections            = var.connections
+#  description            = var.description
+#  glue_version           = var.glue_version
+#  max_retries            = var.max_retries
+#  timeout                = var.timeout
+#  security_configuration = var.security_configuration
+#  worker_type            = var.worker_type
+#  number_of_workers      = var.number_of_workers
+# script_location = var.script_location 
+#  python_version  = var.python_version
+# }
 
 ######################################################################################################
 ### post gress ###
@@ -36,7 +36,7 @@ module "postgresql_cluster" {
   source = "../../modules/postgres"
 
   db_subnet_group_name   = "my-db-subnet-group"
-  subnet_ids             = ["subnet-032f000315efe9124","subnet-0731f902c06ec204c"]
+  subnet_ids             = ["subnet-0731f902c06ec204c"]
   security_group_name    = "testvpckey"
   vpc_id                 = "vpc-07832f2f1eb8d75eb"
   db_instance_identifier = "my-postgresql-db"
