e3a7fbdc37ddd2a5efb936d3c02a7e69f62bd550
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-25 07:57:53 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
@@ -9,4 +9,24 @@ resource "aws_s3_bucket_versioning" "versioning_sample" {
   versioning_configuration {
     status = "Enabled"
   }
+
+resource "aws_s3_bucket_archive" "archieve_bucket" {
+  bucket = var.bucket_name_archieve
+  acl    = "private"
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      days          = var.lifecycle_transition_days_glacier
+      storage_class = "GLACIER"
+    }
+
+    transition {
+      days          = var.lifecycle_transition_days_deep_archive
+      storage_class = "DEEP_ARCHIVE"
+    }
+  }
+}
+  
 }
