b67f6e390d10a15f306f9eabcfbdc43538f0af6e
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-25 08:00:07 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/environments/dev/variables.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/variables.tf
@@ -6,12 +6,20 @@ variable "bucket_name_first" {
   type = string
   description = "Name of the first s3 Bucketb"
 }  
- 
-variable "bucket_name_second" {
+ variable "bucket_name_archieve" {
   type = string
   description = "Name of the second s3 Bucketb"
 }
 
+variable "lifecycle_transition_days_glacier" {
+  description = "Number of days before transitioning to Glacier storage class"
+  default     = 30
+}
+
+variable "lifecycle_transition_days_deep_archive" {
+  description = "Number of days before transitioning to Deep Archive storage class"
+  default     = 60
+}
 variable "database_name" {
   description = "The name of the database to create within the PostgreSQL instance"
   default     = "exampledb"
