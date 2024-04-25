44b81eefc5f172c852909fab635eb485bbee9b05
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-25 07:59:06 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/variables.tf
@@ -3,7 +3,18 @@ variable "bucket_name_first" {
   description = "Name of the first s3 Bucketb"
 }  
  
-variable "bucket_name_second" {
+variable "bucket_name_archieve" {
   type = string
   description = "Name of the second s3 Bucketb"
 }
+
+variable "lifecycle_transition_days_glacier" {
+  description = "Number of days before transitioning to Glacier storage class"
+  default     = 30
+}
+
+variable "lifecycle_transition_days_deep_archive" {
+  description = "Number of days before transitioning to Deep Archive storage class"
+  default     = 60
+}
+
