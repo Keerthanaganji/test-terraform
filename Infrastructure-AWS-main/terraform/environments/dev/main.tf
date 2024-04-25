7ad590a817f428d9d252bfa0839ccfc56b1a7587
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-25 11:56:51 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-  region = var.region
-}
+#provider "aws" {
+#  region = var.region
+#}
 
-module "s3_bucket" {
-  source = "../../modules/s3_bucket"
+#module "s3_bucket" {
+#  source = "../../modules/s3_bucket"
  
-  bucket_name_first = var.bucket_name_first
-  bucket_name_archieve = var.bucket_name_archieve
-}
+#  bucket_name_first = var.bucket_name_first
+#  bucket_name_archieve = var.bucket_name_archieve
+#}
 
 ######################################################################################################
 ### glue Job ###
