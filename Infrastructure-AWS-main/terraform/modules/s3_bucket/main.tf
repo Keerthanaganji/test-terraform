a36836b782626c9b570150cf9a03b01c3acb2c3f
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-25 12:16:34 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/main.tf
@@ -16,29 +16,15 @@ resource "aws_s3_bucket" "log_bucket" {
   acl    = "private"
 
 
-  tiering {
-    access_tier = "ARCHIVE_ACCESS"
-    days        = 125
-  }
-  tiering {
-    access_tier = "DEEP_ARCHIVE_ACCESS"
-    days        = 180
+    lifecycle_rule {
+    id      = "example-rule"
+    enabled = true
+
+    transition {
+      days          = 125
+      storage_class = "INTELLIGENT_TIERING"
+    }
   }
 }
 
- resource "aws_s3_bucket" "raw_bucket" {
-  bucket = var.bucket_name_raw
-  acl    = "private"
-
-
-  tiering {
-    access_tier = "ARCHIVE_ACCESS"
-    days        = 125
-  }
-  tiering {
-    access_tier = "DEEP_ARCHIVE_ACCESS"
-    days        = 180
-  }
-
-}
 
