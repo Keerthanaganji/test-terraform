dd8b3ef64397cd89fce073bec2b53ed9d72905a0
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-25 07:45:12 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -28,14 +28,25 @@ provider "aws" {
 # script_location = var.script_location 
 #  python_version  = var.python_version
 # }
+##########################################################################################################
 
-######################################################################################################
-### post gress ###
-################################################################################################
-module "postgresql_cluster" {
-  source = "../../modules/postgres"
-  
+module "postgresql" {
+  source  = "../../modules/postgres"
+
+  db_subnet_group_name           = "my-db-subnet-group"
+  vpc_id                         = "vpc-07832f2f1eb8d75eb"
+  db_subnet_group_subnet_ids     = ["subnet-03a665b3db2978814","subnet-0731f902c06ec204c"]
+  allocated_storage              = 20
+  engine                         = "postgres"
+  engine_version                 = "12.4"
+  instance_class                 = "db.t3.medium"
+  name                           = "my-postgresql"
+  username                       = "postgres"
+  password                       = "your_password"
+  publicly_accessible            = false
+  skip_final_snapshot            = true
 }
+
 
 
   
