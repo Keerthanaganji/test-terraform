36d2a8b9b157af0ba61d95e7287131a45e499a25
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 20:34:47 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -16,11 +16,6 @@ resource "aws_glue_security_configuration" "example_security_configuration" {
 
 resource "aws_glue_job" "job" {
   name                   = var.name
-  connections {
-    vpc_id             = var.vpc_id
-    subnet_ids         = var.subnet_ids
-    security_group_ids = var.security_group_ids
-  }
   role_arn               = "arn:aws:iam::992382526479:role/glue_etl_job"
   description            = var.description
   max_retries            = var.max_retries
