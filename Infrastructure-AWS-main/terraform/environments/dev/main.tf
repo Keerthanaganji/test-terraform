dc55cda954f9a403e14ff60c67805120c3e3722f
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 18:12:29 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -45,5 +45,7 @@ module "postgres_cluster" {
   subnet_ids               = ["subnet-03a665b3db2978814", "subnet-0731f902c06ec204c"]  # Provide your existing subnet IDs here
   security_group_ids       = ["sg-030c0f8e77df31572", "sg-028f561ebcf411e7a"]  # Provide your existing security group IDs here
   subnet_group_name  = "my_db_subnet_group22"
+  master_username          = "admin"  # Change to your desired master username
+  master_password          = "Password123!" 
 }
 
