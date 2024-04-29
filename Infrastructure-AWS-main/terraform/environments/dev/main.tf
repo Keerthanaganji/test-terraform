e65f79db77be29769ec0d7aeb196463af349327a
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 14:26:39 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -33,7 +33,15 @@ module "aws_glue_job" {
 #####postgresss#######
 ############################################################################################
 
+module "aws_vpc" {
+  source = "../../modules/postgres"
 
+  name                 = "demovpc"
+  cidr                 = "10.0.0.0/16"
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
 
 
 
