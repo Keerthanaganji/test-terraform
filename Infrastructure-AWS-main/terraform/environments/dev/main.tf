231ba02616f8feb76194de0dd34b48221655ee25
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 18:37:19 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -13,21 +13,22 @@ provider "aws" {
 ### glue Job ###
 ####################################################################################################
 
-#module "aws_glue_job" {
-#  source = "../../modules/glue_job"
+module "aws_glue_job" {
+  source = "../../modules/glue_job"
 
- # name              = var.name
- # connections            = var.connections
-#  description            = var.description
-#  glue_version           = var.glue_version
-#  max_retries            = var.max_retries
-#  timeout                = var.timeout
-#  security_configuration = var.security_configuration
-#  worker_type            = var.worker_type
-#  number_of_workers      = var.number_of_workers
- # script_location = var.script_location 
-#  python_version  = var.python_version
-#}
+  name              = var.name
+  connections            = var.connections
+  description            = var.description
+  glue_version           = var.glue_version
+  max_retries            = var.max_retries
+  timeout                = var.timeout
+  security_configuration = var.security_configuration
+  worker_type            = var.worker_type
+  number_of_workers      = var.number_of_workers
+  script_location = var.script_location 
+  security_configuration = aws_glue_security_configuration.example_security_configuration.name  # Assuming you have a security configuration created
+  python_version  = var.python_version
+}
 
 ################################################################################
 # RDS Aurora Module
