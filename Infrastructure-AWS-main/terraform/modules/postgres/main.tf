e7c0d1410a575e8ac8f61c2a44612ecd1810b6da
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 17:03:18 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/main.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/main.tf
@@ -2,7 +2,7 @@ resource "aws_rds_cluster" "testcluster" {
   cluster_identifier      = var.cluster_name
   allocated_storage       = var.allocated_storage
   db_cluster_parameter_group_name = "my_db_cluster_parameter_group"
-  db_subnet_group_name    = "my_db_subnet_group"
+  db_subnet_group_name    = var.subnet_group_name
   vpc_security_group_ids  = var.security_group_ids
 
   engine           = var.engine
