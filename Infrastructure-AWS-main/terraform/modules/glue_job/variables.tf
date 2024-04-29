b1c3ac511cacb406619bbba58a7fd804270ae51a
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 18:39:53 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/variables.tf
@@ -10,10 +10,6 @@ variable "description" {
   description = "The description of the Glue job"
 }
 
-variable "glue_version" {
-  description = "The Glue version to be used for the job"
-}
-
 variable "max_retries" {
   description = "The maximum number of times to retry the job if it fails"
 }
