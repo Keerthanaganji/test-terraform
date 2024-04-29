0fc81ef350c8a8c9ec8122f3e9cb08163eefc5d8
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 11:24:48 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/postgres/main.tf
+++ Infrastructure-AWS-main/terraform/modules/postgres/main.tf
@@ -19,15 +19,14 @@ resource "aws_security_group" "rds" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  // Define egress rules
   egress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = [0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 resource "aws_db_instance" "demotest" {
