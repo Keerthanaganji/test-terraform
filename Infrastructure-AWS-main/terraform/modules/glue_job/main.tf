a61279ce5c3e25cb33bbd847e91aa26d92d4d3c2
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 19:10:04 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -11,9 +11,7 @@ resource "aws_glue_security_configuration" "example_security_configuration" {
     s3_encryption {
       s3_encryption_mode = "DISABLED"
     }
-    job_bookmarks_encryption_mode = "DISABLED"
-    cloudwatch_encryption_mode    = "DISABLED"
-    s3_encryption_mode            = "DISABLED"
+   
     connection_to_resources {
       subnet_ids         = ["subnet-03a665b3db2978814", "subnet-0731f902c06ec204c"]
       security_group_ids = ["sg-030c0f8e77df31572", "sg-028f561ebcf411e7a"]
