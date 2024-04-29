3a8694ee94503475a66cd0df6e4c014c0fc92843
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-29 18:30:12 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -1,3 +1,22 @@
+resource "aws_glue_security_configuration" "example_security_configuration" {
+  name                 = "example-glue-security-configuration"
+  encryption_configuration {
+    cloudwatch_encryption {
+      cloudwatch_encryption_mode = "DISABLED"
+    }
+    job_bookmarks_encryption {
+      job_bookmarks_encryption_mode = "DISABLED"
+    }
+    s3_encryption {
+      s3_encryption_mode = "DISABLED"
+    }
+    connection_to_resources {
+      subnet_ids         = ["subnet-123456789", "subnet-987654321"]  # Specify your subnet IDs
+      security_group_ids = ["sg-0123456789abcdef0"]  # Specify your security group IDs
+    }
+  }
+}
+
 resource "aws_glue_job" "job" {
   name                   = var.name
   role_arn               = "arn:aws:iam::992382526479:role/glue_etl_job"
