fee19e870e2f730ce9e806b5ef18325295f32165
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-04-30 08:44:27 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -38,7 +38,7 @@ module "glue_connection" {
   name = "testconnection"
   s3_output_location = "s3://mybucket/path/to/output.py"
   security_groups_ids = ["sg-030c0f8e77df31572", "sg-028f561ebcf411e7a"]
-  subnet_id = ["subnet-03a665b3db2978814", "subnet-0731f902c06ec204c"]
+  subnet_ids = ["subnet-03a665b3db2978814", "subnet-0731f902c06ec204c"]
 }
 
 ################################################################################
