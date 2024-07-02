4b3b98c5133995fa9449e59caa921392c4c23b4d
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-02 12:35:32 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/variables.tf
@@ -1,1 +1,19 @@
+variable "datahub_to_ref" {
+  description = "The name of the Glue job"
+}
 
+variable "max_capacity" {
+  description = "The list of connections used by the Glue job"
+}
+
+variable "description" {
+  description = "The description of the Glue job"
+}
+
+variable "max_retries" {
+  description = "The maximum number of times to retry the job if it fails"
+}
+
+variable "timeout" {
+  description = "The job timeout in minutes"
+}
