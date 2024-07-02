dd50c7cc3a5f8a838d8930ea28645bf3b8c7ae4b
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-02 12:26:00 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/environments/dev/variables.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/variables.tf
@@ -1,22 +1,8 @@
-variable "region" {
-  description = "AWS region"
-}
-
-variable "bucket_name_first" {
-  type = string
-  description = "Name of the first s3 Bucketb"
-}  
- variable "bucket_name_log" {
-  type = string
-  description = "Name of the second s3 Bucketb"
-}
-
 variable "name" {
   description = "The name of the Glue job"
 }
 
-
-variable "connections" {
+variable "max_capacity" {
   description = "The list of connections used by the Glue job"
 }
 
@@ -24,87 +10,10 @@ variable "description" {
   description = "The description of the Glue job"
 }
 
+variable "max_retries" {
+  description = "The maximum number of times to retry the job if it fails"
+}
+
 variable "timeout" {
   description = "The job timeout in minutes"
 }
-variable "max_retries" {
-  type        = number
-  description = "The maximum number of retries for the Glue job"
-}
-
-variable "security_configuration" {
-  description = "The ID of an existing security configuration for the Glue job"
-}
-variable "worker_type" {
-  type    = string
-}
-
-variable "number_of_workers" {
-  description = "The number of workers to allocate for the Glue job"
-}
-variable "use_glue_catalog" {
-  description = "Flag to indicate whether to use Glue Data Catalog as Hive metastore"
-  type        = bool
-  default     = true
-}
-
-variable "script_location" {
-  description = "The S3 path to the script that the Glue job runs"
-}
-
-
-variable "python_version" {
-  type        = number
-  default     = 3
-  description = "(Optional) The Python version being used to execute a Python shell job."
-
-  validation {
-    condition     = contains([2, 3], var.python_version)
-    error_message = "Allowed values are 2 or 3."
-  }
-}
-variable "cluster_name" {
-  description = "The name of the RDS PostgreSQL cluster"
-}
-
-variable "allocated_storage" {
-  description = "The amount of storage (in GB) to be initially allocated for the database"
-  default     = 100
-}
-
-variable "instance_class" {
-  description = "The instance type to use for the RDS PostgreSQL cluster"
-  default     = "db.t3.medium"
-}
-
-variable "engine" {
-  description = "The database engine to use for the RDS PostgreSQL cluster"
-  default     = "aurora-postgresql"
-}
-
-variable "engine_version" {
-  description = "The version of the database engine to use for the RDS PostgreSQL cluster"
-  default     = "10.14"
-}
-
-variable "vpc_id" {
-  description = "The ID of the existing VPC where the RDS PostgreSQL cluster will be created"
-}
-
-variable "subnet_ids" {
-  description = "A list of existing subnet IDs where the RDS PostgreSQL cluster will be deployed"
-  type        = list(string)
-}
-
-variable "security_group_ids" {
-  description = "A list of existing security group IDs for the RDS PostgreSQL cluster"
-  type        = list(string)
-}
-variable "subnet_group_name" {
-  description = "The name of the DB subnet group"
-  type        = string
-}
-
-
-
-
