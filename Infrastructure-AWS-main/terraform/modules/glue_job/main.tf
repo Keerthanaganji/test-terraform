6afed8edba6baa7b145fed10b17cf97f899f544e
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-02 12:47:40 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -3,6 +3,7 @@ resource "aws_glue_job" "datahub_to_ref" {
   description            = var.description
   max_retries            = var.max_retries
   timeout                = var.timeout
+  role_arn             = "arn:aws:iam::381492286378:role/glue_role"
 }
 
 resource "aws_glue_trigger" "example_trigger" {
@@ -10,7 +11,7 @@ resource "aws_glue_trigger" "example_trigger" {
   type = "ON_DEMAND"
 
   actions {
-    job_name = aws_glue_job.example_job.name
+    job_name = aws_glue_job.datahub_ref.name
     arguments = {
       "--object_key"       = "your_object_key_value"
       "--max_workers_key"  = "your_max_workers_key_value"
