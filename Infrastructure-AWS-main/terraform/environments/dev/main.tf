cac62f3d0e5a75a1fc5ddb0d6ece33180605517f
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-02 12:40:14 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/environments/dev/main.tf
+++ Infrastructure-AWS-main/terraform/environments/dev/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 module "aws_glue_job" {
   source = "../../modules/glue_job"
 
-  name              = var.datahub_to_ref
+  name              = var.name
   description            = var.description
   max_retries            = var.max_retries
   timeout                = var.timeout
