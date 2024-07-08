5b9b73da1ed89524907d3212bfd9dbdd869c8660
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 16:23:12 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/endpoints/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/endpoints/variables.tf
@@ -1,1 +1,25 @@
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The vpc_id for Endpoint Creation"
+  type        = string
+}
+
+variable "service_name_endpoint" {
+  description = "The service_name for Endpoint Creation"
+  type        = string
+}
+
+variable "subnet_ids_endpoint" {
+  description = "The subnet_ids for Endpoint Creation"
+  type  = list(string)
+}
+
+variable "security_group_ids_endpoints" {
+  description = "The security_group_ids for Endpoint Creation"
+  type  = list(string)
+}
 
