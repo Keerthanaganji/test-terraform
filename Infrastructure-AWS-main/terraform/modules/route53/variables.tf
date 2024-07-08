a41ca937d42e74504d52764ffe733c8096b07108
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 16:30:47 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/route53/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/route53/variables.tf
@@ -1,1 +1,26 @@
 
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The vpc_id for Route53 Record Creation"
+  type        = string
+}
+
+variable "snowflake_url" {
+  description = "The snowflake_url for Route53 Record Creation"
+  type        = string
+}
+
+variable "ocsp_snowflake_url" {
+  description = "The ocsp_snowflake_url for Route53 Record Creation"
+  type        = string
+}
+
+variable "dns_record" {
+  description = "The dns_record for Route53 Record Creation"
+  type  = list(string)
+}
+
