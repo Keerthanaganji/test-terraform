afe14c5879d72ca221d90dfa66a046a243f3b41f
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 15:51:39 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/athena/main.tf
+++ Infrastructure-AWS-main/terraform/modules/athena/main.tf
@@ -1,1 +1,21 @@
+# Define the provider
+provider "aws" {
+  region = var.region # Specify your desired AWS region
+}
 
+# Create S3 bucket for query results
+resource "aws_s3_bucket" "athena_results_bucket" {
+  bucket = var.athena_bucket
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+     apply_server_side_encryption_by_default {
+       sse_algorithm = "AES256"
+     }
+    }
+  }
+  tags = {
+   Name =  var.athena_bucket
+   Environment = var.environment
+  }
+ }
