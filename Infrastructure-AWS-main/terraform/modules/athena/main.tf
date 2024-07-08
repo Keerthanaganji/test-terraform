2b61261575714460998005c5e65958f63b7cb216
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 15:52:33 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/athena/main.tf
+++ Infrastructure-AWS-main/terraform/modules/athena/main.tf
@@ -19,3 +19,53 @@ resource "aws_s3_bucket" "athena_results_bucket" {
    Environment = var.environment
   }
  }
+
+# Create Athena workgroup
+resource "aws_athena_workgroup" "athena" {
+  name        = var.workgroup_name
+  description = "Athena Workgroup"
+  configuration {
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket. athena_results_bucket.bucket}/query_result/"
+    }
+   }
+  }
+
+resource "aws_athena_database" "database" {
+  name = var.database
+  bucket = aws_s3_bucket.athena_results_bucket.bucket
+}
+resource "aws_glue_crawler" "example" {
+  database_name = aws_athena_database.database.name
+  name          = "AthenaCrawlerRaw"
+  role        = var.gluerole
+
+  s3_target {
+   path = var.crawlerbucketpath
+  }
+}
+
+resource "aws_s3_bucket_policy" "athena_bucket_policy_https" {
+  bucket = aws_s3_bucket.athena_results_bucket.id
+  policy = data.aws_iam_policy_document.athena_bucket_policy_https.json
+}
+
+data "aws_iam_policy_document" "athena_bucket_policy_https" {
+  statement {
+    actions   = ["s3:*"]
+    effect    = "Deny"
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.athena_results_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.athena_results_bucket.bucket}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
