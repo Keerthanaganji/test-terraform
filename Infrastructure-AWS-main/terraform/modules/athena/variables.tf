3c908583c00ee6c945ec7cba61f1b1298c0f3ebb
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 15:53:08 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/athena/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/athena/variables.tf
@@ -1,1 +1,34 @@
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment name"
+}
+
+variable "athena_bucket" {
+  description = "The name of the athena bucket"
+  type        = string
+}
+
+variable "workgroup_name" {
+  description = "The name of the workgroup name"
+  type        = string
+}
+
+variable "database" {
+  type        = string
+  description = "Name of the database"
+}
+
+variable "gluerole" {
+  type        = string
+  description = "Glure role ARN for Athena"
+}
+
+variable "crawlerbucketpath" {
+  type        = string
+  description = "Bucket path for crawler to create tables"
+}
 
