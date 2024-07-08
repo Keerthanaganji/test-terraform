eb4419079c5e9ca2c15b37d9f3a78916149da10d
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 16:22:38 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/endpoints/main.tf
+++ Infrastructure-AWS-main/terraform/modules/endpoints/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+ region = var.region
+}
+
+resource "aws_vpc_endpoint" "privateendpoint" {
+ vpc_id = var.vpc_id
+ service_name = var.service_name_endpoint
+ vpc_endpoint_type = "Interface"
+ subnet_ids = var.subnet_ids_endpoint
+ security_group_ids = var.security_group_ids_endpoints
+}
 
