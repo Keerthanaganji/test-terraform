075c65fcf52ca9b92a482ca054b7e24cffd89e4e
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 16:30:06 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/route53/main.tf
+++ Infrastructure-AWS-main/terraform/modules/route53/main.tf
@@ -1,1 +1,24 @@
+resource "aws_route53_zone" "private" {
+  name = "privatelink.snowflakecomputing.com"
+
+  vpc {
+    vpc_id = var.vpc_id
+  }
+}
+
+resource "aws_route53_record" "hosturlsnowflake" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = var.snowflake_url
+  type    = "CNAME"
+  ttl     = 60
+  records = var.dns_record
+}
+
+resource "aws_route53_record" "hosturloscp" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = var.ocsp_snowflake_url
+  type    = "CNAME"
+  ttl     = 60
+  records = var.dns_record
+}
 
