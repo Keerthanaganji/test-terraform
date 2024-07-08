695e24740ca7005817ba4e1a0779273ac11373c6
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 16:16:09 +0530

    Update variables.tf

--- Infrastructure-AWS-main/terraform/modules/s3_bucket/variables.tf
+++ Infrastructure-AWS-main/terraform/modules/s3_bucket/variables.tf
@@ -1,13 +1,38 @@
-variable "bucket_name_first" {
-  type = string
-  description = "Name of the first s3 Bucketb"
-}  
- 
-variable "bucket_name_log" {
-  type = string
-  description = "Name of the second s3 Bucketb"
+variable "region" {
+  description = "The AWS region"
+  type        = string
 }
 
+variable "environment" {
+  description = "The environment name"
+}
 
+variable "artifact_bucket" {
+  description = "The name of the artifact bucket"
+  type        = string
+}
 
+variable "log_bucket" {
+  description = "The name of the log bucket"
+  type        = string
+}
 
+variable "raw_bucket" {
+  description = "The name of the raw bucket"
+  type        = string
+}
+
+variable "processed_bucket" {
+  description = "The name of the processed bucket"
+  type        = string
+}
+
+variable "limfinity_bucket" {
+  description = "The name of the limfinity bucket"
+  type        = string
+}
+
+variable "identifier" {
+  description = "The ARN for accessing the limfinity bucket"
+  type     = list(string)
+}
