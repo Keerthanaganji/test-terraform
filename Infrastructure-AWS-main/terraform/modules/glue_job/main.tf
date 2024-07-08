f7a50716f9e047eaee4739f0ac4f36bb4239d788
Keerthanaganji <165990523+Keerthanaganji@users.noreply.github.com>
2024-07-08 15:55:20 +0530

    Update main.tf

--- Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
+++ Infrastructure-AWS-main/terraform/modules/glue_job/main.tf
@@ -1,28 +1,51 @@
-resource "aws_glue_job" "datahub_to_ref" {
-  name                   = var.name
+provider "aws" {
+  region = var.region
+}
+
+#resource "aws_cloudwatch_log_group" "gluelog" {
+#  name  = var.log_group_name
+#  retention_in_days = var.retention_in_days
+#}
+
+resource "aws_glue_connection" "example"{
+  name               = var.glue_connection_name    
+  connection_type    = var.connection_type
+  physical_connection_requirements {
+    security_group_id_list = var.glue_security_group_id_list
+    subnet_id              = var.glue_subnet_id
+    availability_zone      = var.availability_zone
+  }
+}
+
+resource "aws_glue_job" "labware8_to_raw" {
+  name                   = var.glue_job_name
+  role_arn               = var.role_arn
   description            = var.description
   max_retries            = var.max_retries
   timeout                = var.timeout
-  role_arn             = "arn:aws:iam::381492286378:role/glue_role"
-
+  max_capacity           = var.max_capacity
 
   command {
-    name               = "python"
-    script_location    = "path/glue_job/"
-    python_version     = 3.9
+    name               = var.job_type_name
+    script_location    = var.labware8_to_raw_script_location
+    python_version     = var.python_version
 }
+  default_arguments = {
+    "--additional-python-modules" = var.additional_python_modules != [] ? join(",", var.additional_python_modules) : null,
+    "--enable-glue-datacatalog"   = var.enable_glue_datacatalog ? "" : null
+   # "--continuous-log-logGroup"  = aws_cloudwatch_log_group.gluelog.name
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"  = "true"
+    "--enable-metrics"    = ""
+    "--extra-py-files"               = length(var.extra_py_files) > 0 ? join(",", var.extra_py_files) : null
+    "--TempDir"                   = length(var.temp_dir) > 0 ? join(",", var.temp_dir) : null
+  
+
 }
 
-resource "aws_glue_trigger" "example_trigger" {
-  name = "example-trigger"
-  type = "ON_DEMAND"
-
-  actions {
-    job_name = "datahub_trigger"
-    arguments = {
-      "--object_key"       = "your_object_key_value"
-      "--max_workers_key"  = "your_max_workers_key_value"
-      "--bucket_key"       = "your_bucket_key_value"
-    }
+  execution_property {
+    max_concurrent_runs = var.max_concurrent_runs # Limit to one concurrent run
   }
+  connections = [aws_glue_connection.example.name]
+
 }
